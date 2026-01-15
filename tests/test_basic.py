a7d42f125484584fb108d8d7ca855cd56906f231
Alon Almog <alon.almog@boomi.com>
2026-01-15 14:48:14 +0200

    test: update version test to 1.2.0

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,7 +7,7 @@ def test_version():
     """Test that version is defined."""
     from efspurge import __version__
 
-    assert __version__ == "1.0.0"
+    assert __version__ == "1.2.0"
 
 
 def test_imports():
